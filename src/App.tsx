771924441b25e65e8aeb844106e4f35b729c4933
Diego Brihuega <diegosfb@gmail.com>
2026-03-27 10:16:21 -0700

    [RELEASE] v2.4.0 (Bump)

--- src/App.tsx
+++ src/App.tsx
@@ -475,7 +475,7 @@ export default function App() {
         {/* Debug Version Info */}
         <div className="fixed bottom-2 right-2 z-50 pointer-events-none opacity-40 transition-opacity">
           <div className="text-xs font-mono text-white text-right uppercase tracking-tighter">
-            v2.3.0-debug | 2026-03-27
+            v2.4.0-debug | 2026-03-27
           </div>
         </div>
       </div>
@@ -899,7 +899,7 @@ export default function App() {
       {/* Debug Version Info */}
       <div className="fixed bottom-2 right-2 z-50 pointer-events-none opacity-40 transition-opacity">
         <div className="text-xs font-mono text-white text-right uppercase tracking-tighter">
-          v2.3.0-debug | 2026-03-27
+          v2.4.0-debug | 2026-03-27
         </div>
       </div>
     </div>
